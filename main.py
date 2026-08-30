efd92745fdaf4a6c94033c6e01920c84231cde66
Heidi Ishin Chen <heidiishinchen@MacBook-Pro-67.local>
2026-08-29 23:59:31 -0700

    Reconfigure URL

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://YOUR-FRONTEND-NAME.onrender.com",  # add this once you know the real URL
+        "https://aic-consulting-bot.onrender.com",  # add this once you know the real URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
